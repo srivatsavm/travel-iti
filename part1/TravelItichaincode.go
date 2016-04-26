c850c363b4fac0ee634a352eccc41742aefa37f8
srivatsavm <srivatsav.m@gmail.com>
2016-04-27 06:08:24 +1000

    popasdf

--- part1/TravelItichaincode.go
+++ part1/TravelItichaincode.go
@@ -228,7 +228,7 @@ func (t *TravelItiChaincode) init_travelIti(stub *shim.ChaincodeStub, args []str
 
 	//str := `{"name": "` + args[0] + `", "color": "` + color + `", "size": ` + strconv.Itoa(balance) + `, "user": "` + user + `","travelid": "` + args[0] + `", "balance": ` + strconv.Itoa(balance) + `,"travelstate": ` + args[0] + `, "stateowner": "` + args[0] + `"}`
 
-	str := `{"name": "` + args[0] + `", "travelid": "` + args[0] + `", "balance": ` + strconv.Itoa(balance) + `,"travelstate": ` + args[0] + `, "stateowner": "` + args[0] + `"}`
+	str := `{"name": "` + args[0] + `"  , "color": "` + color + `" , "travelid": "` + args[0] + `", "balance": ` + strconv.Itoa(balance) + `,"travelstate": ` + args[0] + `, "stateowner": "` + args[0] + `"}`
 
 	
 
