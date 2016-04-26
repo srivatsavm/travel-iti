a499961cd16c01c0e2b3d6796afd63999cae2228
srivatsavm <srivatsav.m@gmail.com>
2016-04-26 15:30:07 +1000

    update7

--- part1/TravelItichaincode.go
+++ part1/TravelItichaincode.go
@@ -1,22 +1,3 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
-
 package main
 
 import (
@@ -37,10 +18,10 @@ var travelItiIndexStr = "_travelItiindex"				//name for the key/value that will 
 var openTradesStr = "_opentrades"				//name for the key/value that will store all open trades
 
 type TravelIti struct{
-	Name string `json:"name"`					//the fieldtags are needed to keep case from bouncing around
-	Color string `json:"color"`
-	Size int `json:"size"`
-	User string `json:"user"`
+	traveId int `json:"travelid"`
+	balance int `json:"balance"`
+	travelstate string `json:"travelstate"`				
+	stateowner string `json:"stateowner"`
 }
 
 // ============================================================================================================================
