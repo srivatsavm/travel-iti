a14037dfdac64ebd848a4eb4e9516c5d359e1d35
srivatsavm <srivatsav.m@gmail.com>
2016-04-27 06:37:04 +1000

    poopop

--- part1/TravelItichaincode.go
+++ part1/TravelItichaincode.go
@@ -16,43 +16,23 @@ type TravelItiChaincode struct {
 var travelItiIndexStr = "_travelItiindex"				
 
 type TravelIti struct{
-	travelid int `json:"travelid"`
+	travelid string `json:"travelid"`
 	balance int `json:"balance"`
 	travelstate string `json:"travelstate"`				
 	stateowner string `json:"stateowner"`
 	}
 
 // ============================================================================================================================
-// Init - reset all the things
-// ============================================================================================================================
-func (t *TravelItiChaincode) init(stub *shim.ChaincodeStub, args []string) ([]byte, error) {
-	var Aval int
-	var err error
-
-	return nil, nil
-}
-
-// ============================================================================================================================
 // Run - Our entry point
 // ============================================================================================================================
 func (t *TravelItiChaincode) Run(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-	fmt.Println("run is running " + function)
-
-	// Handle different functions
-	if function == "init" {													//initialize the chaincode state, used as reset
-		return t.init(stub, args)
-	}
-	fmt.Println("run did not find func: " + function)						//error
-
-	return nil, errors.New("Received unknown function invocation")
+	return nil, nil
 }
-
 
 // ============================================================================================================================
 // Query - read a variable from chaincode state - (aka read)
 // ============================================================================================================================
 func (t *TravelItiChaincode) Query(stub *shim.ChaincodeStub, function string, args []string) ([]byte, error) {
-
 	return nil, nil													//send it onward
 }
 
